f3bbd9a750487bbf8a03ec00437d6e61915c52f3
Uttara Ravi <ur2135@columbia.edu>
2021-04-27 02:44:46 -0400

    fetching form values from filter page

--- frontend/render.js
+++ frontend/render.js
@@ -6,31 +6,49 @@ function secondFunc(){
 	window.location.href='./customer.html';
 }
 
-function populate() {
-    // var img=document.createElement("img");
-    // img.src="https://www.google.co.in/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
-    // img.id="picture"
+// function populate() {
+//     var apigClient = apigClientFactory.newClient({apiKey: "y1yJqKthiV3ceJlZu4Kps6XYcPpq9uf2aHPWOfsY"});
 
-    // let food_div = document.createElement("div")
-    // food_div.classList.add('row');
-    // food_div.classList.add('row-sm');
-    // food_div.h4 = "Falafel";
-    // food_div.price = "10$";
-    // food_div.qty = "3";
-    // var foo = document.getElementById("test1");
-    // foo.appendChild(food_div);
-    var fname = "New Pita bread";
-    var price = "50.00";
-    var qty = "3";
-    var str = '<div class="row-sm row"><h4 id="dname">'+fname+'</h4><h5 id="price">'+price+'</h5><h5 id="qty">Quantity : '+qty+'</h5></div>',
-    // var str = '<p>Some more <span>text</span> here</p>',
-    div = document.getElementById( 'test1' );
-	div.insertAdjacentHTML( 'beforeend', str );
-}
+//     var fname = "New Pita bread";
+//     var price = "50.00";
+//     var qty = "3";
+//     var str = '<div class="row-sm row"><h4 id="dname">'+fname+'</h4><h5 id="price">'+price+'</h5><h5 id="qty">Quantity : '+qty+'</h5></div>',
+//     // var str = '<p>Some more <span>text</span> here</p>',
+//  //    div = document.getElementById( 'test1' );
+// 	// div.insertAdjacentHTML( 'beforeend', str );
+
+//     var additionalParams = {headers: {
+//         'Content-Type':"application/json"
+//     }};
+
+//     var params = {"Content-Type" : "application/json" };
+//     var body = {"username":test,  "emailID":test1};
+
+
+//     apigClient.rootPost(params,body,additionalParams).then(function(res){
+//           console.log(res);
+//             if(res.status==200){
+//             console.log(res);
+//                 document.getElementById('outputXX').value=res;
+
+//         }
+//     });
+
+// }
 
 function add_item(objButton) {
 	alert(objButton.value);
 }
+
+function foodFilter(){
+    var dname = document.getElementById('dname').value;
+    var rname = document.getElementById('rname').value;
+    var dietTag1 = document.getElementById('dietTag1').value;
+    var calories = document.getElementById('max-calories').value;
+}
+
+
+
 
 
 
