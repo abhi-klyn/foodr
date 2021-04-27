350e4f62d1964b75cf6e4ba3025d14cd356292ed
Uttara Ravi <ur2135@columbia.edu>
2021-04-27 02:59:54 -0400

    not much change

--- frontend/render.js
+++ frontend/render.js
@@ -6,35 +6,32 @@ function secondFunc(){
 	window.location.href='./customer.html';
 }
 
-// function populate() {
-//     var apigClient = apigClientFactory.newClient({apiKey: "y1yJqKthiV3ceJlZu4Kps6XYcPpq9uf2aHPWOfsY"});
+function populate() {
+    
 
-//     var fname = "New Pita bread";
-//     var price = "50.00";
-//     var qty = "3";
-//     var str = '<div class="row-sm row"><h4 id="dname">'+fname+'</h4><h5 id="price">'+price+'</h5><h5 id="qty">Quantity : '+qty+'</h5></div>',
-//     // var str = '<p>Some more <span>text</span> here</p>',
-//  //    div = document.getElementById( 'test1' );
-// 	// div.insertAdjacentHTML( 'beforeend', str );
+    var fname = "New Pita bread";
+    var price = "50.00";
+    var qty = "3";
+    var str = '<div class="row-sm row"><h4 id="dname">'+fname+'</h4><h5 id="price">'+price+'</h5><h5 id="qty">Quantity : '+qty+'</h5></div>',
 
-//     var additionalParams = {headers: {
-//         'Content-Type':"application/json"
-//     }};
+    var additionalParams = {headers: {
+        'Content-Type':"application/json"
+    }};
 
-//     var params = {"Content-Type" : "application/json" };
-//     var body = {"username":test,  "emailID":test1};
+    var params = {"Content-Type" : "application/json" };
+    var body = {"username":test,  "emailID":test1};
 
 
-//     apigClient.rootPost(params,body,additionalParams).then(function(res){
-//           console.log(res);
-//             if(res.status==200){
-//             console.log(res);
-//                 document.getElementById('outputXX').value=res;
+    apigClient.rootPost(params,body,additionalParams).then(function(res){
+          console.log(res);
+            if(res.status==200){
+            console.log(res);
+                document.getElementById('outputXX').value=res;
 
-//         }
-//     });
+        }
+    });
 
-// }
+}
 
 function add_item(objButton) {
 	alert(objButton.value);
@@ -45,6 +42,28 @@ function foodFilter(){
     var rname = document.getElementById('rname').value;
     var dietTag1 = document.getElementById('dietTag1').value;
     var calories = document.getElementById('max-calories').value;
+
+    var apigClient = apigClientFactory.newClient(
+        {apiKey: "y1yJqKthiV3ceJlZu4Kps6XYcPpq9uf2aHPWOfsY"}
+    );
+
+    var additionalParams = {headers: {
+        'Content-Type':"application/json"
+    }};
+
+    var params = {"Content-Type" : "application/json" };
+    var body = {"username":dname,  "emailID":rname};
+    // var body = {"rName":rname, "foodName":dname,"tag":"","calories":""};
+    apigClient.rootPost(params,body,additionalParams).then(function(res){
+          console.log(res);
+            if(res.status==200){
+                console.log('res : ', res);
+                document.getElementById('outputXX').value=res;
+            }
+    });
+
+
+
 }
 
 
