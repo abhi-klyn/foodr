c440480c545337e8d64dd08af5597ccdbda3129d
Abhishek Kalyan Adupa <abhishek.adupa@columbia.edu>
2021-04-28 00:33:33 -0400

    test

--- frontend/assets/js/sdk/apigClient.js
+++ frontend/assets/js/sdk/apigClient.js
@@ -155,6 +155,42 @@ apigClientFactory.newClient = function (config) {
     };
     
     
+    apigClient.checkoutPost = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var checkoutPostRequest = {
+            verb: 'post'.toUpperCase(),
+            path: pathComponent + uritemplate('/checkout').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(checkoutPostRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.checkoutOptions = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var checkoutOptionsRequest = {
+            verb: 'options'.toUpperCase(),
+            path: pathComponent + uritemplate('/checkout').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(checkoutOptionsRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
     apigClient.searchPost = function (params, body, additionalParams) {
         if(additionalParams === undefined) { additionalParams = {}; }
         
